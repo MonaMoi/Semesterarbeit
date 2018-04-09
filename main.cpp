0aa1f005e65aff3291633697815c089fe5a4410e
marquitoh <markus.haberl95@googlemail.com>
2018-04-09 19:08:57 +0200

    edit menu control

--- main.cpp
+++ main.cpp
@@ -14,9 +14,42 @@ int main() {
 
     input.open(filename);
 
+
+
+back:
+    cout << "############FILM SAMMLUNG############" << endl;
+    cout << "#####################################" << endl;
+    cout << "(s)uche " << endl;
+    cout << "(e)xportiere " << endl;
+    cout << "(z)eige " << endl;
+    cout << "(h)ilfe" << endl;
+    cout << "(b)eenden" << endl;
+    cout << "#####################################" << endl;
+    char eingabe;
+
+    cin >> eingabe;
+    switch (eingabe){
+    case 's':
+        break;
+    case 'e':
+        break;
+    case 'z':
+        break;
+    case 'h':
+        break;
+    case 'b':
+        input.close();
+        return 0;
+        break;
+    default:
+        cout << "Falsche Eingabe, druecke h fuer Hilfe" << endl;
+        break;
+    }
+
+    goto back;
+
     while (!input.eof()) {
-        parser->parseXMLData(input);
-       // parser->parseMetaData(input);
+        parser->parseMetaData(input);
     }
     input.close();
 
